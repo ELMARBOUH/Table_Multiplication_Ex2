27e378aa27d98a56c79b99bb77c640cfe0c12230
KHALID ELMARBOUH <khalid.Elmarbouh.devloper@gmail.com>
2023-01-21 19:16:05 +0100

    Activity_Home.java :
    Add Method OnClick
    
    Linked Button btn_reinitialiser & btn_quitter

--- app/src/main/java/com/example/ex_2/Activity_Home.java
+++ app/src/main/java/com/example/ex_2/Activity_Home.java
@@ -2,13 +2,46 @@ package com.example.ex_2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Switch;
 
 public class Activity_Home extends AppCompatActivity {
+    EditText et_Number;
+    Button btn_reinitialiser , btn_Afficher , btn_quitter ;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        et_Number = findViewById(R.id.et_Number);
+        btn_reinitialiser = findViewById(R.id.btn_reinitialiser);
+        btn_quitter = findViewById(R.id.btn_quitter);
+
+
+        btn_reinitialiser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                et_Number.setText("");
+            }
+
+        });
+
+
+        btn_quitter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+
+            }
+        });
+
     }
+
 }
