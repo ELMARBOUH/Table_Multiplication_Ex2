b8fba0713865e56dfc5e9d2074f7c8c03485239e
KHALID ELMARBOUH <khalid.Elmarbouh.devloper@gmail.com>
2023-01-21 22:20:38 +0100

    Activity_Home.java
    Created Onclick Bacghround & Switch Background

--- app/src/main/java/com/example/ex_2/Activity_Home.java
+++ app/src/main/java/com/example/ex_2/Activity_Home.java
@@ -3,19 +3,24 @@ package com.example.ex_2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.Switch;
 
 public class Activity_Home extends AppCompatActivity {
     EditText et_Number;
-    Button btn_reinitialiser , btn_Afficher , btn_quitter ;
+    Button btn_reinitialiser , btn_Afficher , btn_quitter ,btn_pink , btn_white , btn_purple ;
+    View layout ;
+    
 
 
 
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,6 +28,10 @@ public class Activity_Home extends AppCompatActivity {
         et_Number = findViewById(R.id.et_Number);
         btn_reinitialiser = findViewById(R.id.btn_reinitialiser);
         btn_quitter = findViewById(R.id.btn_quitter);
+        btn_pink = findViewById(R.id.btn_pink);
+        btn_white = findViewById(R.id.btn_white);
+        btn_purple = findViewById(R.id.btn_purple);
+        layout = findViewById(R.id.layout);
 
 
         btn_reinitialiser.setOnClickListener(new View.OnClickListener() {
@@ -41,6 +50,35 @@ public class Activity_Home extends AppCompatActivity {
 
             }
         });
+
+
+        btn_pink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                    layout.setBackgroundColor(Color.parseColor("#E5BEBE"));
+            }
+        });
+
+        btn_purple.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                layout.setBackgroundColor(Color.parseColor("#E3ACF9"));
+            }
+        });
+
+        btn_white.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                layout.setBackgroundColor(Color.parseColor("#FFFFFFFF"));
+            }
+        });
+
+
+
+
+
+
+
 
     }
 
